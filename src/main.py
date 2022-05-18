21665bae98b9d6bf618d5059a5508bab2db38a20
Sewens <Sewen@mail.dlut.edu.cn>
2022-05-18 10:11:19 +0800

    Update main.py
    测试

--- src/main.py
+++ src/main.py
@@ -22,19 +22,19 @@ if __name__ == '__main__':
     browser.maximize_window()
 
     # 用户名密码配置信息, 可选多人
-    user_id = 'xxxx'
-    password = '****'
+    user_id = '11809048'
+    password = 'dutir412341435'
     email = 'xxxx@163.com'
     user_id_n = 'xxxx'
     password_n = '****'
     email_n = 'xxxx@163.com'
 
     # 输入需要的图书馆和座位候选座位
-    wanted_seats = [239, 235] # 请输入完整的3位座位号（如不足请用0补足）
+    wanted_seats = [63] # 请输入完整的3位座位号（如不足请用0补足）
     library_name = '令希' # 请输入伯川或令希
-    reading_room = 401 # 请预先在系统上确定要指定的阅览室
+    reading_room = 302 # 请预先在系统上确定要指定的阅览室
 
-    user1 = Reserve(user_id_n, password_n, wanted_seats, email_n, library_name, reading_room)
+    user1 = Reserve(user_id, password, wanted_seats, email_n, library_name, reading_room)
     # user2 = Reserve(user_id, password, wanted_seats, email, library_name, reading_room)
 
     user1.reserve(browser)
